9dc743d0b7cae522b7d530a5aa48337419c8efc8
Allen Shibu <93600615+alllenshibu@users.noreply.github.com>
2023-10-15 19:08:38 +0530

    Game 4

---
+++ src/pages/api/game-4/index.js
@@ -0,0 +1,17 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import generateUniqueFlag from "@/utils/UniqueFlag";
+
+const game4FlagStaticPart = "flag{dfsafewcvascd";
+
+export default function handler(req, res) {
+  if (req.method !== "GET") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  const newFlag = generateUniqueFlag();
+
+  const flag = `${game4FlagStaticPart}${newFlag}}}}`;
+
+  return res.status(200).json({ flag: flag });
+}
