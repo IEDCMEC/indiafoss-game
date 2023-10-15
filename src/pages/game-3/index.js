d52af5b8e23966defd0b547dffc2ee10565b18ea
Allen Shibu <93600615+alllenshibu@users.noreply.github.com>
2023-10-15 19:08:19 +0530

    Game 3

---
+++ src/pages/game-3/index.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import generateUniqueFlag from "@/utils/UniqueFlag";
+
+const game3FlagStaticPart = "flag{dfsafewcvascd";
+
+export default function Game3() {
+  const [flag, setFlag] = useState("");
+
+  const fetchUniqueFlag = () => {
+    const newFlag = generateUniqueFlag();
+    setFlag(`${game3FlagStaticPart}${newFlag}}`);
+  };
+
+  useEffect(() => {
+    document.cookie = `flag=${flag};path=/game-3`;
+  }, [flag]);
+
+  useEffect(() => {
+    fetchUniqueFlag();
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <h1>Cookie check</h1>
+      </div>
+    </div>
+  );
+}
