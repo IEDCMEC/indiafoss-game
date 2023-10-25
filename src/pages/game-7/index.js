79868040e88e3f09e8d0f4089482d5ec83d911ba
Subru-37 <mdl21ec119@mec.ac.in>
2023-10-26 01:25:31 +0530

    added basic ui for easy levels

--- src/pages/game-7/index.js
+++ src/pages/game-7/index.js
@@ -3,7 +3,10 @@ import { useRouter } from "next/router";
 
 import generateUniqueFlag from "@/utils/UniqueFlag";
 import { supabaseClient } from "@/utils/supabase";
-
+import { Box } from "@chakra-ui/react";
+import CustomForm from "@/Components/CustomForm";
+import { Heading } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 import { useTimer } from "@/contexts/Timer";
 
 const gameScore = 7;
@@ -63,32 +66,89 @@ export default function Game7() {
   };
 
   return (
-    <div>
-      <div>
-        <h1>Sup</h1>
-        {timer}
-      </div>
-      <form>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          name="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          id="password"
-          type="password"
-          name="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <input type="submit" value="Submit" onClick={handleFormSubmit} />
-      </form>
-      <div>
-        <label htmlFor="submission">Flag</label>
+    // <div>
+    //   <div>
+    //     <h1>Sup</h1>
+    //     {timer}
+    //   </div>
+    //   <form>
+    //     <label htmlFor="username">Username</label>
+    //     <input
+    //       id="username"
+    //       type="text"
+    //       name="username"
+    //       value={username}
+    //       onChange={(e) => setUsername(e.target.value)}
+    //     />
+    //     <label htmlFor="password">Password</label>
+    //     <input
+    //       id="password"
+    //       type="password"
+    //       name="password"
+    //       value={password}
+    //       onChange={(e) => setPassword(e.target.value)}
+    //     />
+    //     <input type="submit" value="Submit" onClick={handleFormSubmit} />
+    //   </form>
+    //   <div>
+    //     <label htmlFor="submission">Flag</label>
+    //     <input
+    //       id="submission"
+    //       type="text"
+    //       value={submission}
+    //       onChange={(e) => {
+    //         setSubmission(e.target.value);
+    //       }}
+    //     />
+    //     <button onClick={handleFlagSubmit}>Submit</button>
+    //   </div>
+    // </div>
+    <Box
+      backgroundColor="#AEDEFC"
+      height="100vh"
+      width="100vw"
+      flexDirection={{ base: "column", md: "row" }}
+      display="flex"
+      alignItems="center"
+      justifyContent="space-around"
+    >
+      <Box
+        flexDirection={"column"}
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        width={{ base: "95vw", sm: "400px" }}
+        border="2px solid #190482"
+        borderRadius={"md"}
+        padding={"30px 0"}
+        minHeight="300px"
+        // sx={{
+        //   '&:hover':{
+        //     border: '2px solid #190482'
+        //   }
+        // }}
+      >
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent={"center"}
+          flexDirection={"column"}
+          // mb={5}
+        >
+          <Heading as="h2" size="xl">
+            Sup
+          </Heading>
+          <p>Time Left: {timer}</p>
+        </Box>
+        <form onSubmit={handleFlagSubmit}>
+          <Box
+            display="flex"
+            alignItems="center"
+            justifyContent={"center"}
+            flexDirection={"column"}
+            width={"350px"}
+          >
+            {/* <label htmlFor="submission">Flag</label>
         <input
           id="submission"
           type="text"
@@ -96,9 +156,62 @@ export default function Game7() {
           onChange={(e) => {
             setSubmission(e.target.value);
           }}
-        />
-        <button onClick={handleFlagSubmit}>Submit</button>
-      </div>
-    </div>
+        /> */}
+            <CustomForm
+              id="submission"
+              type="text"
+              value={submission}
+              label="Submit the flag"
+              onChange={(e) => {
+                setSubmission(e.target.value);
+              }}
+            />
+            <Button colorScheme="teal" type="submit">
+              Submit
+            </Button>
+          </Box>
+        </form>
+      </Box>
+      <form onSubmit={handleFormSubmit}>
+        <Box
+          flexDirection={"column"}
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+          width={{ base: "95vw", sm: "400px" }}
+          border="2px solid #190482"
+          borderRadius={"md"}
+          padding={"30px 0"}
+          height="300px"
+        >
+          <Box
+            display="flex"
+            alignItems="center"
+            justifyContent={"center"}
+            flexDirection={"column"}
+            width={"350px"}
+            height="100%"
+          >
+            <CustomForm
+              id="username"
+              type="text"
+              label="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <CustomForm
+              id="password"
+              type="text"
+              label="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button colorScheme="teal" type="submit">
+              Submit
+            </Button>
+          </Box>
+        </Box>
+      </form>
+    </Box>
   );
 }
