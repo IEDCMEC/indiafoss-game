d2edf349dd801b7dd4f330d18d8870ace6694e76
Jaison Dennis <jaisondennis080@gmail.com>
2023-10-26 14:08:12 +0530

    Bugs Fixed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,8 @@ import "@/styles/globals.css";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { Toaster } from "react-hot-toast";
 import { TimerProvider } from "@/contexts/Timer";
+import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   const theme = extendTheme({
@@ -13,7 +15,24 @@ export default function App({ Component, pageProps }) {
   return (
     <TimerProvider>
       <ChakraProvider theme={theme}>
-        <Toaster position="top-right"/>
+        <Toaster position="top-right" />
+        <Head>
+          <title>Game | IEDC MEC</title>
+          <Script
+            strategy="lazyOnload"
+            src={`https://www.googletagmanager.com/gtag/js?id=G-DY0PT292WB`}
+          />
+          <Script strategy="lazyOnload">
+            {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-DY0PT292WB', {
+        page_path: window.location.pathname,
+        });
+    `}
+          </Script>
+        </Head>
         <Component {...pageProps} />
       </ChakraProvider>
     </TimerProvider>
