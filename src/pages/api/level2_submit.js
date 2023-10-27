5b28e200c234b4ae5ed5685e5875ecd70038b540
Jaison Dennis <jaisondennis080@gmail.com>
2023-10-27 07:20:15 +0530

    Level Hard Submission Page

---
+++ src/pages/api/level2_submit.js
@@ -0,0 +1,36 @@
+import { supabaseClient } from "@/utils/supabase";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const {
+      body: { name, email, phoneNumber, flag },
+    } = req;
+
+    if (!name || !email || !flag) {
+      return res.status(400).json({
+        error: "Missing name or email or flag",
+      });
+    }
+
+    const { data, error } = await supabaseClient
+      .from("level2_players")
+      .insert([
+        {
+          name,
+          email,
+          phone: phoneNumber,
+          flag,
+        },
+      ]);
+
+      console.log(data, error);
+
+    return res.status(200).json({
+        message: "Submission Successful",
+    });
+  } else {
+    return res.status(405).json({
+      error: "Method not allowed",
+    });
+  }
+}
