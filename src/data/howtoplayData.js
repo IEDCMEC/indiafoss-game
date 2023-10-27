f13d32ca43342d324c83113ce41d4849dffe5f93
Jaison Dennis <jaisondennis080@gmail.com>
2023-10-27 09:34:47 +0530

    Added Instructions

---
+++ src/data/howtoplayData.js
@@ -0,0 +1,12 @@
+export const howtoPlayData = [
+    "At the beginning of each round, you will be presented with a set of seven letters that you can use to form words.",
+    "You can move the letters around to form different words, but you must only use the seven letters listed.",
+    "The first 3 valid words may contain any of the 7 listed letters",
+    "However, the words submitted after the first 3 MUST mandatorily contain the center letter.",
+    "Each word you submit must contain at least four letters.",
+    "You can submit as many words as you like within the time limit of each round.",
+    "The new set of letters will be revealed everyday at 08:00 AM IST.",
+    "The time limit for each game is 24 hours.",
+    "Each valid word will be given points based on the number of letters and also extra 5 points would be awarded if you submit a pangram.",
+    "There is no limit to the number of attempts allowed.",
+  ];
--- src/pages/index.js
+++ src/pages/index.js
@@ -69,108 +69,110 @@ function Home() {
   };
 
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-      height="100vh"
-      backgroundColor="#c2d0dd"
-    >
-      <Heading as="h1" size="2xl" mb={6}>
-        Capture The Flag 🚩 !
-      </Heading>
+    <>
       <Box
-        width={{ base: "95vw", md: "100%" }}
-        maxW="400px"
-        p={4}
-        borderWidth={1}
-        borderRadius="md"
-        boxShadow="md"
-        border="2px solid #094074"
-        mb={"50px"}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        height="100vh"
+        backgroundColor="#c2d0dd"
       >
-        <form onSubmit={handleSubmit}>
-          <FormControl id="name" isRequired mb={4}>
-            <FormLabel>Name</FormLabel>
-            <Input
-              type="text"
-              value={formData.name}
-              onChange={(e) =>
-                setFormData({ ...formData, name: e.target.value })
-              }
-              border="2px solid #094074"
+        <Heading as="h1" size="2xl" mb={6}>
+          Capture The Flag 🚩 !
+        </Heading>
+        <Box
+          width={{ base: "95vw", md: "100%" }}
+          maxW="400px"
+          p={4}
+          borderWidth={1}
+          borderRadius="md"
+          boxShadow="md"
+          border="2px solid #094074"
+          mb={"50px"}
+        >
+          <form onSubmit={handleSubmit}>
+            <FormControl id="name" isRequired mb={4}>
+              <FormLabel>Name</FormLabel>
+              <Input
+                type="text"
+                value={formData.name}
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
+                border="2px solid #094074"
+                sx={{
+                  "&:hover": {
+                    border: "2px solid #190482",
+                  },
+                }}
+              />
+              {validationErrors.name && (
+                <Alert status="error" mt={2}>
+                  <AlertIcon />
+                  {validationErrors.name}
+                </Alert>
+              )}
+            </FormControl>
+
+            <FormControl id="email" isRequired mb={4}>
+              <FormLabel>Email</FormLabel>
+              <Input
+                type="email"
+                value={formData.email}
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
+                border="2px solid #094074"
+                sx={{
+                  "&:hover": {
+                    border: "2px solid #190482",
+                  },
+                }}
+              />
+              {validationErrors.email && (
+                <Alert status="error" mt={2}>
+                  <AlertIcon />
+                  {validationErrors.email}
+                </Alert>
+              )}
+            </FormControl>
+
+            <FormControl id="phoneNumber" mb={4}>
+              <FormLabel>Phone Number</FormLabel>
+              <Input
+                type="text"
+                value={formData.phoneNumber}
+                onChange={(e) =>
+                  setFormData({ ...formData, phoneNumber: e.target.value })
+                }
+                border="2px solid #094074"
+                sx={{
+                  "&:hover": {
+                    border: "2px solid #190482",
+                  },
+                }}
+              />
+            </FormControl>
+
+            <Button
+              type="submit"
+              backgroundColor="#094074"
               sx={{
                 "&:hover": {
-                  border: "2px solid #190482",
+                  backgroundColor: "#094074",
                 },
               }}
-            />
-            {validationErrors.name && (
-              <Alert status="error" mt={2}>
-                <AlertIcon />
-                {validationErrors.name}
-              </Alert>
-            )}
-          </FormControl>
-
-          <FormControl id="email" isRequired mb={4}>
-            <FormLabel>Email</FormLabel>
-            <Input
-              type="email"
-              value={formData.email}
-              onChange={(e) =>
-                setFormData({ ...formData, email: e.target.value })
-              }
-              border="2px solid #094074"
-              sx={{
-                "&:hover": {
-                  border: "2px solid #190482",
-                },
-              }}
-            />
-            {validationErrors.email && (
-              <Alert status="error" mt={2}>
-                <AlertIcon />
-                {validationErrors.email}
-              </Alert>
-            )}
-          </FormControl>
-
-          <FormControl id="phoneNumber" mb={4}>
-            <FormLabel>Phone Number</FormLabel>
-            <Input
-              type="text"
-              value={formData.phoneNumber}
-              onChange={(e) =>
-                setFormData({ ...formData, phoneNumber: e.target.value })
-              }
-              border="2px solid #094074"
-              sx={{
-                "&:hover": {
-                  border: "2px solid #190482",
-                },
-              }}
-            />
-          </FormControl>
-
-          <Button
-            type="submit"
-            backgroundColor="#094074"
-            sx={{
-              "&:hover": {
-                backgroundColor: "#094074",
-              },
-            }}
-            color="white"
-            w={"100%"}
-          >
-            {loading ? <PulseLoader color={"#ffffff"} size={10} /> : "Submit"}
-          </Button>
-        </form>
+              color="white"
+              w={"100%"}
+            >
+              {loading ? <PulseLoader color={"#ffffff"} size={10} /> : "Submit"}
+            </Button>
+          </form>
+        </Box>
+        <Footer />
       </Box>
-      <Footer />
-    </Box>
+    </>
   );
 }
 
