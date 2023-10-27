6af4758357c94708286aa6f4d5a16c519b660d1d
Allen Shibu <93600615+alllenshibu@users.noreply.github.com>
2023-10-27 21:30:26 +0530

    Add hint and server ip for level 2

--- src/pages/level2.js
+++ src/pages/level2.js
@@ -8,6 +8,8 @@ import {
   Button,
   Alert,
   AlertIcon,
+  Text,
+  Link,
 } from "@chakra-ui/react";
 import axios from "axios";
 import toast from "react-hot-toast";
@@ -75,117 +77,157 @@ function Level2Submission() {
       <Heading as="h1" size="2xl" mb={6}>
         {"Capture The Flag (Hard)🚩 !"}
       </Heading>
+
       <Box
-        width={{ base: "95vw", md: "100%" }}
-        maxW="400px"
-        p={4}
-        borderWidth={1}
-        borderRadius="md"
-        boxShadow="md"
-        border="2px solid #094074"
-        mb={"50px"}
+        display="flex"
+        flexDirection={{ base: "column", md: "row" }}
+        alignItems="center"
+        justifyContent="center"
+        gap={6}
+        width="100%"
       >
-        <form onSubmit={handleSubmit}>
-          <FormControl id="name" isRequired mb={4}>
-            <FormLabel>Name</FormLabel>
-            <Input
-              type="text"
-              value={formData.name}
-              onChange={(e) =>
-                setFormData({ ...formData, name: e.target.value })
-              }
-              border="2px solid #094074"
-              sx={{
-                "&:hover": {
-                  border: "2px solid #190482",
-                },
-              }}
-            />
-            {validationErrors.name && (
-              <Alert status="error" mt={2}>
-                <AlertIcon />
-                {validationErrors.name}
-              </Alert>
-            )}
-          </FormControl>
-
-          <FormControl id="email" isRequired mb={4}>
-            <FormLabel>Email</FormLabel>
-            <Input
-              type="email"
-              value={formData.email}
-              onChange={(e) =>
-                setFormData({ ...formData, email: e.target.value })
-              }
-              border="2px solid #094074"
-              sx={{
-                "&:hover": {
-                  border: "2px solid #190482",
-                },
-              }}
-            />
-            {validationErrors.email && (
-              <Alert status="error" mt={2}>
-                <AlertIcon />
-                {validationErrors.email}
-              </Alert>
-            )}
-          </FormControl>
-
-          <FormControl id="phoneNumber" mb={4}>
-            <FormLabel>Phone Number</FormLabel>
-            <Input
-              type="text"
-              value={formData.phoneNumber}
-              onChange={(e) =>
-                setFormData({ ...formData, phoneNumber: e.target.value })
-              }
-              border="2px solid #094074"
-              sx={{
-                "&:hover": {
-                  border: "2px solid #190482",
-                },
-              }}
-            />
-          </FormControl>
-
-          <FormControl id="flag" isRequired mb={4}>
-            <FormLabel>Flag</FormLabel>
-            <Input
-              type="text"
-              value={formData.flag}
-              onChange={(e) =>
-                setFormData({ ...formData, flag: e.target.value })
-              }
-              border="2px solid #094074"
-              sx={{
-                "&:hover": {
-                  border: "2px solid #190482",
-                },
-              }}
-            />
-            {validationErrors.flag && (
-              <Alert status="error" mt={2}>
-                <AlertIcon />
-                {validationErrors.flag}
-              </Alert>
-            )}
-          </FormControl>
-
-          <Button
-            type="submit"
-            backgroundColor="#094074"
-            sx={{
-              "&:hover": {
-                backgroundColor: "#094074",
-              },
-            }}
-            color="white"
-            w={"100%"}
+        <Box
+          width={{ base: "95vw", md: "100%" }}
+          maxW="400px"
+          p={4}
+          borderWidth={1}
+          borderRadius="md"
+          boxShadow="md"
+          border="2px solid #094074"
+          mb={"50px"}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Text textAlign="center" fontSize={{ base: "1rem", md: "1.1rem" }}>
+            Uncover the flag's covert sanctuary in the server's binary realm,
+            where the cryptic guardian weaves secrets within the digital
+            tapestry. To reveal the hidden standard-bearer, decipher the
+            enigmatic echoes, for each mark and cipher is a note in the symphony
+            of knowledge, unlocking the prized secret.
+          </Text>
+          <Link
+            textAlign="center"
+            textColor={"darkred"}
+            href="http://20.219.107.74"
+            isExternal
           >
-            Submit Flag
-          </Button>
-        </form>
+            20.219.107.74
+          </Link>
+        </Box>
+        <Box
+          width={{ base: "95vw", md: "100%" }}
+          maxW="400px"
+          p={4}
+          borderWidth={1}
+          borderRadius="md"
+          boxShadow="md"
+          border="2px solid #094074"
+          mb={"50px"}
+        >
+          <form onSubmit={handleSubmit}>
+            <FormControl id="name" isRequired mb={4}>
+              <FormLabel>Name</FormLabel>
+              <Input
+                type="text"
+                value={formData.name}
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
+                border="2px solid #094074"
+                sx={{
+                  "&:hover": {
+                    border: "2px solid #190482",
+                  },
+                }}
+              />
+              {validationErrors.name && (
+                <Alert status="error" mt={2}>
+                  <AlertIcon />
+                  {validationErrors.name}
+                </Alert>
+              )}
+            </FormControl>
+
+            <FormControl id="email" isRequired mb={4}>
+              <FormLabel>Email</FormLabel>
+              <Input
+                type="email"
+                value={formData.email}
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
+                border="2px solid #094074"
+                sx={{
+                  "&:hover": {
+                    border: "2px solid #190482",
+                  },
+                }}
+              />
+              {validationErrors.email && (
+                <Alert status="error" mt={2}>
+                  <AlertIcon />
+                  {validationErrors.email}
+                </Alert>
+              )}
+            </FormControl>
+
+            <FormControl id="phoneNumber" mb={4}>
+              <FormLabel>Phone Number</FormLabel>
+              <Input
+                type="text"
+                value={formData.phoneNumber}
+                onChange={(e) =>
+                  setFormData({ ...formData, phoneNumber: e.target.value })
+                }
+                border="2px solid #094074"
+                sx={{
+                  "&:hover": {
+                    border: "2px solid #190482",
+                  },
+                }}
+              />
+            </FormControl>
+
+            <FormControl id="flag" isRequired mb={4}>
+              <FormLabel>Flag</FormLabel>
+              <Input
+                type="text"
+                value={formData.flag}
+                onChange={(e) =>
+                  setFormData({ ...formData, flag: e.target.value })
+                }
+                border="2px solid #094074"
+                sx={{
+                  "&:hover": {
+                    border: "2px solid #190482",
+                  },
+                }}
+              />
+              {validationErrors.flag && (
+                <Alert status="error" mt={2}>
+                  <AlertIcon />
+                  {validationErrors.flag}
+                </Alert>
+              )}
+            </FormControl>
+
+            <Button
+              type="submit"
+              backgroundColor="#094074"
+              sx={{
+                "&:hover": {
+                  backgroundColor: "#094074",
+                },
+              }}
+              color="white"
+              w={"100%"}
+            >
+              Submit Flag
+            </Button>
+          </form>
+        </Box>
       </Box>
       <Footer />
     </Box>
